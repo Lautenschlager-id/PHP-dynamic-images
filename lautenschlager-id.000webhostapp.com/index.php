ae1f1d61531b8ec921ca0c2a000b19cd1fc52e68
Lautenschlager <tai.romani@hotmail.com>
2019-01-07 18:47:21 -0200

    Add files via upload

--- lautenschlager-id.000webhostapp.com/index.php
+++ lautenschlager-id.000webhostapp.com/index.php
@@ -1,9 +1,9 @@
 <?php
 $translations = [
 	"en" => [ utf8_decode("Ended %a days, %H hours, and %i minutes ago.")      , utf8_decode("%a days, %H hours, and %i minutes left.")      ],
-	"br" => [ utf8_decode("Finalizado h? %a dias, %H horas e %i minutos.")     , utf8_decode("%a dias, %H horas e %i minutos restantes.")    ],
+	"br" => [ utf8_decode("Finalizado há %a dias, %H horas e %i minutos.")     , utf8_decode("%a dias, %H horas e %i minutos restantes.")    ],
 	"es" => [ utf8_decode("Finalizó hace %a días, %H horas y %i minutos.")     , utf8_decode("Quedan %a días, %H horas y %i minutos.")       ],
-	"fr" => [ utf8_decode("Termin? depuis %a jours, %H heures et %i minutes.") , utf8_decode("%a jours, %H heures et %i minutes restantes.") ],
+	"fr" => [ utf8_decode("Terminé depuis %a jours, %H heures et %i minutes.") , utf8_decode("%a jours, %H heures et %i minutes restantes.") ],
 	"pl" => [ utf8_decode("Zakonczono %a dni, %H godzin i %i minut temu.")     , utf8_decode("Pozostalo %a dni, %H godzin i %i minut.")      ]
 ];
 $translation = (empty($_GET["lang"]) || empty($translations[$_GET["lang"]]))  ? $translations["en"] : $translations[$_GET["lang"]];
